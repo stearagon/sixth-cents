8f1706230c1448b16f354fa9b0c901b14f5718bd
stearagon <stearagon@gmail.com>
2015-07-14 11:32:18 -0400

    Create authenticity form helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def form_auth
+    <<-HTML
+      <input type="hidden" name="authenticity_token"
+      value="#{ form_authenticity_token }">
+    HTML
+    .html_safe
+  end
 end
